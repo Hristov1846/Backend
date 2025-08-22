13eaa847041f35bd43268bc03f18aa1bb63608bc
Hristov1846 <Hristov1846@mail.bg>
2025-08-22 22:44:20 +0300

    storyController.js

--- controllers/storyController.js
+++ controllers/storyController.js
@@ -1,33 +1,19 @@
 const Story = require("../models/Story");
 
-// Създаване на стори
 exports.createStory = async (req, res) => {
   try {
-    const story = new Story({
-      user: req.user.id,
-      image: req.body.image,
-      video: req.body.video,
-      visibleToCloseFriends: req.body.visibleToCloseFriends
-    });
-    await story.save();
+    const story = await Story.create({ ...req.body, user: req.user.id });
     res.status(201).json(story);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 };
 
-// Реакция на стори
-exports.reactStory = async (req, res) => {
+exports.getStories = async (req, res) => {
   try {
-    const { reaction } = req.body;
-    const story = await Story.findById(req.params.id);
-    story.reactions.push({ user: req.user.id, reaction });
-    if (!story.viewers.includes(req.user.id)) {
-      story.viewers.push(req.user.id);
-    }
-    await story.save();
-    res.json(story);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
+    const stories = await Story.find({ expiresAt: { $gt: Date.now() } }).populate("user", "username profilePicture");
+    res.json(stories);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 };
