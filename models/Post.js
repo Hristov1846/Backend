6051586b3b785405a5723ad9afb5a1c79ab37845
Hristov1846 <Hristov1846@mail.bg>
2025-08-22 22:37:53 +0300

    Post.js

--- models/Post.js
+++ models/Post.js
@@ -1,20 +1,28 @@
 const mongoose = require("mongoose");
 
-const postSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  content: { type: String },
-  image: { type: String },
-  video: { type: String },
-  likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-  reactions: [{
-    user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-    type: { type: String } // ❤️ 😆 😮 😢 😡
-  }],
-  comments: [{
-    user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-    text: String,
-    createdAt: { type: Date, default: Date.now }
-  }]
-}, { timestamps: true });
+const postSchema = new mongoose.Schema(
+  {
+    author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    text: { type: String, default: "" },
+    media: { type: String, default: "" },
+
+    likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    reactions: [
+      {
+        user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+        type: { type: String, enum: ["like", "love", "haha", "wow", "sad", "angry"] },
+      },
+    ],
+
+    comments: [
+      {
+        user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+        text: { type: String, required: true },
+        createdAt: { type: Date, default: Date.now },
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Post", postSchema);
