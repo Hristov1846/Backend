30b8da603f06b48a1c2542d744d3e404ab6efa45
Hristov1846 <Hristov1846@mail.bg>
2025-08-22 22:47:38 +0300

    notificationController.js

--- controllers/notificationController.js
+++ controllers/notificationController.js
@@ -1,21 +1,22 @@
-const Notification = require("../models/Notification");
+const Wallet = require("../models/Wallet");
 
-// Взимане на известия
-exports.getNotifications = async (req, res) => {
+exports.getWallet = async (req, res) => {
   try {
-    const notifications = await Notification.find({ user: req.user.id }).sort({ createdAt: -1 });
-    res.json(notifications);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
+    const wallet = await Wallet.findOne({ user: req.user.id });
+    res.json(wallet);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 };
 
-// Маркиране като прочетени
-exports.markAsRead = async (req, res) => {
+exports.deposit = async (req, res) => {
   try {
-    await Notification.updateMany({ user: req.user.id, isRead: false }, { isRead: true });
-    res.json({ message: "All marked as read" });
-  } catch (err) {
-    res.status(500).json({ message: err.message });
+    const wallet = await Wallet.findOne({ user: req.user.id });
+    wallet.balance += req.body.amount;
+    wallet.transactions.push({ type: "deposit", amount: req.body.amount });
+    await wallet.save();
+    res.json(wallet);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 };
