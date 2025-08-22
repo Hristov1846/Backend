777b48c1153b142e5ed3108765c527f6672decaa
Hristov1846 <Hristov1846@mail.bg>
2025-08-22 22:47:03 +0300

    walletController.js

--- controllers/walletController.js
+++ controllers/walletController.js
@@ -1,29 +1,22 @@
 const Wallet = require("../models/Wallet");
-const Transaction = require("../models/Transaction");
 
-// Взимане на баланс
-exports.getBalance = async (req, res) => {
+exports.getWallet = async (req, res) => {
   try {
     const wallet = await Wallet.findOne({ user: req.user.id });
     res.json(wallet);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 };
 
-// Депозит
 exports.deposit = async (req, res) => {
   try {
-    const { amount } = req.body;
     const wallet = await Wallet.findOne({ user: req.user.id });
-    wallet.balance += amount;
+    wallet.balance += req.body.amount;
+    wallet.transactions.push({ type: "deposit", amount: req.body.amount });
     await wallet.save();
-
-    const transaction = new Transaction({ user: req.user.id, amount, type: "deposit" });
-    await transaction.save();
-
     res.json(wallet);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 };
