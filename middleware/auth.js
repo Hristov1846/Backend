e5097e6189d8a9c456de5f0b93bdff21b8281878
Hristov1846 <Hristov1846@mail.bg>
2025-08-22 22:33:56 +0300

    auth.js

---
+++ middleware/auth.js
@@ -0,0 +1,17 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = function (req, res, next) {
+  const token = req.header("Authorization")?.replace("Bearer ", "");
+
+  if (!token) {
+    return res.status(401).json({ message: "Няма токен, достъпът е отказан" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded.id;
+    next();
+  } catch (error) {
+    res.status(400).json({ message: "Невалиден токен" });
+  }
+};
