cb4eb416f9c9a263036910df7c3abaa59d305ab0
Hristov1846 <Hristov1846@mail.bg>
2025-08-22 22:40:53 +0300

    Notification.js

--- models/Notification.js
+++ models/Notification.js
@@ -1,10 +1,14 @@
 const mongoose = require("mongoose");
 
-const notificationSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  type: { type: String, required: true }, // like, comment, follow, gift, message...
-  message: { type: String },
-  isRead: { type: Boolean, default: false }
-}, { timestamps: true });
+const notificationSchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    type: { type: String, required: true }, // like, comment, gift, follow, etc.
+    message: { type: String },
+    link: { type: String },
+    isRead: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Notification", notificationSchema);
