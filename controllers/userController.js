cb45855423184e204065ac872a263b60f7a3005d
Hristov1846 <Hristov1846@mail.bg>
2025-08-22 22:42:41 +0300

    userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,37 +1,19 @@
 const User = require("../models/User");
 
-// Взимане на профил
 exports.getUserProfile = async (req, res) => {
   try {
-    const user = await User.findById(req.params.id).populate("friends followers following wallet");
+    const user = await User.findById(req.params.id).select("-password");
     res.json(user);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 };
 
-// Актуализиране на профил
 exports.updateProfile = async (req, res) => {
   try {
-    const updates = req.body;
-    const user = await User.findByIdAndUpdate(req.user.id, updates, { new: true });
-    res.json(user);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-};
-
-// Добавяне на приятел
-exports.addFriend = async (req, res) => {
-  try {
-    const { friendId } = req.body;
-    const user = await User.findById(req.user.id);
-    if (!user.friends.includes(friendId)) {
-      user.friends.push(friendId);
-      await user.save();
-    }
-    res.json({ message: "Friend added" });
-  } catch (err) {
-    res.status(500).json({ message: err.message });
+    const updatedUser = await User.findByIdAndUpdate(req.user.id, req.body, { new: true });
+    res.json(updatedUser);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 };
