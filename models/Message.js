baab450265c0aa979ed7dd01ebe715e1456d5f63
Hristov1846 <Hristov1846@mail.bg>
2025-08-22 22:39:12 +0300

    Message.js

--- models/Message.js
+++ models/Message.js
@@ -1,12 +1,14 @@
 const mongoose = require("mongoose");
 
-const messageSchema = new mongoose.Schema({
-  chat: { type: mongoose.Schema.Types.ObjectId, ref: "Chat" },
-  sender: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  text: { type: String },
-  image: { type: String },
-  video: { type: String },
-  temporary: { type: Boolean, default: false } // временни снимки
-}, { timestamps: true });
+const messageSchema = new mongoose.Schema(
+  {
+    chatId: { type: mongoose.Schema.Types.ObjectId, ref: "Chat", required: true },
+    sender: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    text: { type: String },
+    media: { type: String }, // снимка/видео
+    isTemporary: { type: Boolean, default: false }, // временни снимки
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Message", messageSchema);
