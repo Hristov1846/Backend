ea4f9296aa1293fa43a3d99bbe4bd80de937dbae
Hristov1846 <Hristov1846@mail.bg>
2025-08-22 22:40:20 +0300

    Wallet.js

--- models/Wallet.js
+++ models/Wallet.js
@@ -1,8 +1,20 @@
 const mongoose = require("mongoose");
 
-const walletSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  balance: { type: Number, default: 0 }
-}, { timestamps: true });
+const walletSchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    balance: { type: Number, default: 0 },
+    transactions: [
+      {
+        type: { type: String, enum: ["deposit", "withdraw", "gift", "earn"], required: true },
+        amount: { type: Number, required: true },
+        date: { type: Date, default: Date.now },
+        from: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+        to: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Wallet", walletSchema);
