a30a0a13febccb19b11d28fc15cd912afcd5fc45
Hristov1846 <Hristov1846@mail.bg>
2025-08-23 14:09:00 +0300

    chatRoutes.js

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const router = express.Router();
-const { createChat, sendMessage } = require("../controllers/chatController");
-const auth = require("../middleware/auth");
+const chatController = require("../controllers/chatController");
+// Тук оправяме името на middleware-а
+const auth = require("../middleware/authMiddleware");
 
-router.post("/create", auth, createChat);
-router.post("/send", auth, sendMessage);
+// Създаване на чат
+router.post("/", auth, chatController.createChat);
+
+// Вземане на всички чатове на потребител
+router.get("/", auth, chatController.getUserChats);
+
+// Вземане на конкретен чат по ID
+router.get("/:chatId", auth, chatController.getChatById);
+
+// Добавяне на потребител в чат
+router.post("/:chatId/addUser", auth, chatController.addUserToChat);
+
+// Премахване на потребител от чат
+router.post("/:chatId/removeUser", auth, chatController.removeUserFromChat);
 
 module.exports = router;
