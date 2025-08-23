5080cf9fec07377b6cd813355f5dff5ae47a4fc6
Hristov1846 <Hristov1846@mail.bg>
2025-08-24 01:46:52 +0300

    authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,18 +1,19 @@
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 
-const protect = async (req, res, next) => {
+const authMiddleware = async (req, res, next) => {
   let token;
 
   if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
     try {
       token = req.headers.authorization.split(" ")[1];
+
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
       req.user = await User.findById(decoded.id).select("-password");
-
       next();
     } catch (error) {
+      console.error("Auth Error:", error);
       res.status(401).json({ message: "Not authorized, token failed" });
     }
   }
@@ -22,4 +23,4 @@ const protect = async (req, res, next) => {
   }
 };
 
-export default protect;
+export default authMiddleware;
