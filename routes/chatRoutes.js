f2a1481c3e9e058ff9c19a15f346fb9b94326ae5
Hristov1846 <Hristov1846@mail.bg>
2025-08-23 14:13:11 +0300

    chatRoutes.js

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -1,22 +1,27 @@
 const express = require("express");
 const router = express.Router();
-const chatController = require("../controllers/chatController");
-// Тук оправяме името на middleware-а
-const auth = require("../middleware/authMiddleware");
+const {
+  createChat,
+  getUserChats,
+  getChatById,
+  addUserToChat,
+  removeUserFromChat,
+} = require("../controllers/chatController");
+const authMiddleware = require("../middleware/authMiddleware");
 
 // Създаване на чат
-router.post("/", auth, chatController.createChat);
+router.post("/", authMiddleware, createChat);
 
 // Вземане на всички чатове на потребител
-router.get("/", auth, chatController.getUserChats);
+router.get("/", authMiddleware, getUserChats);
 
 // Вземане на конкретен чат по ID
-router.get("/:chatId", auth, chatController.getChatById);
+router.get("/:chatId", authMiddleware, getChatById);
 
 // Добавяне на потребител в чат
-router.post("/:chatId/addUser", auth, chatController.addUserToChat);
+router.post("/:chatId/addUser", authMiddleware, addUserToChat);
 
 // Премахване на потребител от чат
-router.post("/:chatId/removeUser", auth, chatController.removeUserFromChat);
+router.post("/:chatId/removeUser", authMiddleware, removeUserFromChat);
 
 module.exports = router;
