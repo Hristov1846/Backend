024fbe9b5d50bb771aeacd4f1200189f63bd1252
Hristov1846 <Hristov1846@mail.bg>
2025-08-24 01:44:46 +0300

    User.js

--- models/User.js
+++ models/User.js
@@ -3,19 +3,10 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema(
   {
     firstName: { type: String, required: true },
-    middleName: { type: String },
-    lastName: { type: String, required: true },
+    lastName: { type: String },
     email: { type: String, required: true, unique: true },
-    phone: { type: String, required: true, unique: true },
-    birthDate: { type: Date, required: true },
     password: { type: String, required: true },
-    avatar: { type: String, default: "" },
-    coverPhoto: { type: String, default: "" },
-    friends: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-    followers: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-    following: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-    walletBalance: { type: Number, default: 0 },
-    role: { type: String, enum: ["user", "admin"], default: "user" }
+    role: { type: String, enum: ["user", "admin"], default: "user" },
   },
   { timestamps: true }
 );
