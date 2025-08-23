f5441f0e4e9d863e9ee9f36f6265809ba9249ba3
Hristov1846 <Hristov1846@mail.bg>
2025-08-24 01:28:24 +0300

    authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,16 @@
 import express from "express";
-import { registerUser, loginUser } from "../controllers/authController.js";
+import { registerUser, loginUser, getUserProfile } from "../controllers/authController.js";
+import authMiddleware from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
+// Регистрация
 router.post("/register", registerUser);
+
+// Логин
 router.post("/login", loginUser);
+
+// Профил (изисква токен)
+router.get("/profile", authMiddleware, getUserProfile);
 
 export default router;
