741d9af298e6eae61b87331888ffb71467a1ec94
Hristov1846 <Hristov1846@mail.bg>
2025-08-24 00:37:17 +0300

    Message.js

--- models/Message.js
+++ models/Message.js
@@ -1,14 +1,15 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
 const messageSchema = new mongoose.Schema(
   {
-    chatId: { type: mongoose.Schema.Types.ObjectId, ref: "Chat", required: true },
     sender: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    receiver: { type: mongoose.Schema.Types.ObjectId, ref: "User" }, // за 1:1 чат
+    group: { type: mongoose.Schema.Types.ObjectId, ref: "Group" },   // за групов чат (по желание)
     text: { type: String },
-    media: { type: String }, // снимка/видео
-    isTemporary: { type: Boolean, default: false }, // временни снимки
+    media: { type: String }, // линк към изображение/видео
+    isTemporary: { type: Boolean, default: false }, // временни снимки като в Instagram
   },
   { timestamps: true }
 );
 
-module.exports = mongoose.model("Message", messageSchema);
+export default mongoose.model("Message", messageSchema);
