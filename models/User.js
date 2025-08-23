b29b446c41ff3f1de4706962f0e57e22274f7d8a
Hristov1846 <Hristov1846@mail.bg>
2025-08-24 00:24:12 +0300

    User.js

--- models/User.js
+++ models/User.js
@@ -1,5 +1,4 @@
-const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
@@ -8,38 +7,17 @@ const userSchema = new mongoose.Schema(
     lastName: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     phone: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
     birthDate: { type: Date, required: true },
-
+    password: { type: String, required: true },
     avatar: { type: String, default: "" },
     coverPhoto: { type: String, default: "" },
-
-    bio: { type: String, default: "" },
-
     friends: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
     followers: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
     following: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-
     walletBalance: { type: Number, default: 0 },
-
-    professionalMode: { type: Boolean, default: false },
-
-    notifications: [{ type: mongoose.Schema.Types.ObjectId, ref: "Notification" }],
+    role: { type: String, enum: ["user", "admin"], default: "user" }
   },
   { timestamps: true }
 );
 
-// Хеширане на парола преди save
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
-});
-
-// Проверка на парола
-userSchema.methods.matchPassword = async function (enteredPassword) {
-  return await bcrypt.compare(enteredPassword, this.password);
-};
-
-module.exports = mongoose.model("User", userSchema);
+export default mongoose.model("User", userSchema);
