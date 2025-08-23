f20d01fbdf4cb0408d739ff3820071b859d8ff9d
Hristov1846 <Hristov1846@mail.bg>
2025-08-24 01:46:09 +0300

    Post.js

--- models/Post.js
+++ models/Post.js
@@ -1,29 +1,10 @@
 import mongoose from "mongoose";
 
-const commentSchema = new mongoose.Schema(
-  {
-    user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-    text: { type: String, required: true },
-  },
-  { timestamps: true }
-);
-
 const postSchema = new mongoose.Schema(
   {
-    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    text: { type: String, required: true },
-    media: { type: String }, // снимка или видео URL
-    likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-    reactions: [
-      {
-        user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-        type: {
-          type: String,
-          enum: ["like", "love", "haha", "wow", "sad", "angry"],
-        },
-      },
-    ],
-    comments: [commentSchema],
+    author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    content: { type: String, required: true },
+    image: { type: String },
   },
   { timestamps: true }
 );
