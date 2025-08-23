3a060bafa7897e2c20d5fb84ac5cc37998cff349
Hristov1846 <Hristov1846@mail.bg>
2025-08-23 14:23:42 +0300

    messageRoutes.js

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const router = express.Router();
-const { sendMessage, getMessages } = require("../controllers/messageController");
+const { getMessages } = require("../controllers/messageController");
 const authMiddleware = require("../middleware/authMiddleware");
 
-router.post("/", authMiddleware, sendMessage);
+const router = express.Router();
+
+// Всички съобщения в чат
 router.get("/:chatId", authMiddleware, getMessages);
 
 module.exports = router;
