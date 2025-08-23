7c2f102e610b551acbd1ab1bfba5ee0628adae46
Hristov1846 <Hristov1846@mail.bg>
2025-08-23 14:29:36 +0300

    walletController.js

--- controllers/walletController.js
+++ controllers/walletController.js
@@ -1,22 +1,13 @@
-const Wallet = require("../models/Wallet");
+const express = require("express");
+const { addFunds, getBalance } = require("../controllers/walletController");
+const authMiddleware = require("../middleware/authMiddleware");
 
-exports.getWallet = async (req, res) => {
-  try {
-    const wallet = await Wallet.findOne({ user: req.user.id });
-    res.json(wallet);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
+const router = express.Router();
 
-exports.deposit = async (req, res) => {
-  try {
-    const wallet = await Wallet.findOne({ user: req.user.id });
-    wallet.balance += req.body.amount;
-    wallet.transactions.push({ type: "deposit", amount: req.body.amount });
-    await wallet.save();
-    res.json(wallet);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
+// Добавяне на средства
+router.post("/add", authMiddleware, addFunds);
+
+// Взимане на баланс
+router.get("/balance", authMiddleware, getBalance);
+
+module.exports = router;
