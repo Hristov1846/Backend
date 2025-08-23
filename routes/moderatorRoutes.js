57b3fa78f4a67dd985ecf63a5c2c272ff13646ee
Hristov1846 <Hristov1846@mail.bg>
2025-08-23 14:24:13 +0300

    moderatorRoutes.js

--- routes/moderatorRoutes.js
+++ routes/moderatorRoutes.js
@@ -1,44 +1,13 @@
 const express = require("express");
-const router = express.Router();
-const Moderator = require("../models/Moderator");
+const { addModerator, removeModerator } = require("../controllers/moderatorController");
 const authMiddleware = require("../middleware/authMiddleware");
 
+const router = express.Router();
+
 // Добавяне на модератор
-router.post("/add", authMiddleware, async (req, res) => {
-  try {
-    const { moderatorId } = req.body;
-    const newMod = new Moderator({
-      owner: req.user.id,
-      moderator: moderatorId,
-    });
-    await newMod.save();
-    res.json({ message: "Модераторът е добавен успешно!", newMod });
-  } catch (err) {
-    res.status(500).json({ error: "Грешка при добавяне на модератор" });
-  }
-});
+router.post("/add", authMiddleware, addModerator);
 
 // Премахване на модератор
-router.delete("/remove/:id", authMiddleware, async (req, res) => {
-  try {
-    await Moderator.findOneAndDelete({
-      owner: req.user.id,
-      moderator: req.params.id,
-    });
-    res.json({ message: "Модераторът е премахнат успешно!" });
-  } catch (err) {
-    res.status(500).json({ error: "Грешка при премахване на модератор" });
-  }
-});
-
-// Извличане на всички модератори за даден owner
-router.get("/list", authMiddleware, async (req, res) => {
-  try {
-    const mods = await Moderator.find({ owner: req.user.id }).populate("moderator", "name email");
-    res.json(mods);
-  } catch (err) {
-    res.status(500).json({ error: "Грешка при зареждане на модераторите" });
-  }
-});
+router.delete("/remove/:id", authMiddleware, removeModerator);
 
 module.exports = router;
