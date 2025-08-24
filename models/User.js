f9b5f0a08caf5b34c6a1b76079beef25afd56a39
Hristov1846 <Hristov1846@mail.bg>
2025-08-24 17:12:47 +0300

    User.js

--- models/User.js
+++ models/User.js
@@ -2,11 +2,9 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
-    firstName: { type: String, required: true },
-    lastName: { type: String },
-    email: { type: String, required: true, unique: true },
+    username: { type: String, required: true, trim: true },
+    email: { type: String, required: true, unique: true, lowercase: true },
     password: { type: String, required: true },
-    role: { type: String, enum: ["user", "admin"], default: "user" },
   },
   { timestamps: true }
 );
